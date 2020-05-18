6e25a9bf6c7dabaf1e8340e9c691461616b157a2
Dvir Monajem <dmenajem@gmail.com>
2020-05-18 15:18:53 +0300

    products

--- client/src/app/services/products.service.ts
+++ client/src/app/services/products.service.ts
@@ -3,11 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { config } from '../../main-config'
 import { Observable } from 'rxjs';
 import { ProductModel } from '../models/product-model';
+import { map } from 'rxjs/operators';
 
 
 export interface Category {
-  _id : string,
-  name : string
+  _id: string,
+  name: string
 }
 
 @Injectable({
@@ -21,11 +22,26 @@ export class ProductsService {
     private http: HttpClient
   ) { }
 
-  public getProductsCategories() : Observable<Category[]> {
+  public getProductsCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.baseUrl + "/categories")
   }
 
-  public getProductsByCategory(categoryId) : Observable<ProductModel[]> {
-    return this.http.get<ProductModel[]>(this.baseUrl + `/category/${categoryId}`)
+  public getProductsByCategory(categoryId): Observable<ProductModel[]> {
+    return this.http.get<ProductModel[]>(this.baseUrl + `/category/${categoryId}`).pipe(
+      map(response => {
+        const collection = []
+        const temp = [...response]
+        for (let i = 0; i <= response.length;) {
+          const row: ProductModel[] = temp.splice(0, 4)
+          collection.push(row)
+          i = i+4
+        }  
+        return collection
+      })
+    )
+  }
+
+  public deleteProducts(arrId: string[]) {
+    return this.http.delete("url")
   }
 }
