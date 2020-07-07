7c93b02c7b0c4dd2f67b5b60a01bad320c52c73e
Dvir Monajem <dmenajem@gmail.com>
2020-07-07 13:52:19 +0300

    environment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false, 
   server: "http://localhost:3000",
-  imageBucket : "https://products-images-bucket.s3.eu-west-3.amazonaws.com",
+  imageBucket : "http://localhost:3000/uploads/products",
   googlePlaceKey : "AIzaSyDNwLciwS7wLtcjwVZ61nIOZuArCHALCJw",
   googleReCaptchaKey : "6Ld15KIZAAAAAK5Nc3mOrAYiw2KRM5gVfuhaAB-x",
   productLandingPage: "/products/beverages/5e91e29b9c08fc560ce2cf32"
