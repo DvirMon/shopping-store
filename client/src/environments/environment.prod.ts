c8123811c571c546f69e860729da20214d1aa364
Dvir Monajem <dmenajem@gmail.com>
2021-06-26 22:16:25 +0300

    strip api production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   googleClientId  : '806810831298-7hl33u4kuackd8jo284ldov9m4oq6j43.apps.googleusercontent.com',
   googlePlaceKey : "AIzaSyDw-LR1ItISpEOLrRvV5TFtGghoB1ADJBw",
   productLandingPage: "/products/produce/5ec135327088eb479cfcadfc",
-  reCaptchaSiteKey : "6LdY98AZAAAAAI6af--NI-GiebEaf-VBinzL0-ee"
-
+  reCaptchaSiteKey : "6LdY98AZAAAAAI6af--NI-GiebEaf-VBinzL0-ee",
+  stripeApi : 'pk_test_51IwieHHuUxhGUCsGMOeTNsN92hcpP6d40Nt8ACz5tPVZmlmjdo7pDOb5i8mNli78hfTU4MPOTs2GuWPC2OoQMj7K008OFH7hne'
 };
